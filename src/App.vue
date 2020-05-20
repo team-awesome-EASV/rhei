5926d437ff178598fd97ceb500fbe76deac52fae
Raul-Octavina Albu <alburaul@gmil.com>
2020-05-20 15:01:35 +0200

    still working on the login form, some global class were created for dislpay, padding, link styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app">
-    <router-view />
-    <div id="nav">
-      <router-link to="/moodtracker">Moodtracker</router-link>
-      <router-link to="/breathing-exercise">Breathing exercice</router-link>
-      <router-link to="/">Home</router-link>
-      <router-link to="/chat">Chat</router-link>
-      <router-link to="/therapy">Therapy</router-link>
+  <div id="app" class="fullscreen">
+    <div class="main-view">
+      <router-view />
+    </div>
+    <div id="nav" class="flex-space-around padding-sides">
+      <router-link class="margin-right" to="/moodtracker">Mood</router-link>
+      <router-link class="margin-right" to="/breathing-exercise">Breath</router-link>
+      <router-link class="margin-right" to="/">Home</router-link>
+      <router-link class="margin-right" to="/chat">Chat</router-link>
+      <router-link class="margin-right" to="/therapy">Therapy</router-link>
     </div>
   </div>
 </template>
@@ -15,13 +17,48 @@
 :root {
   font-size: 10px;
 }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 #app {
+  position: relative;
+  padding: 3rem;
+}
+
+.main-view {
+  flex-grow: 1;
+  flex: 1;
+}
+.fullscreen {
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
+  position: relative;
 }
 
 #nav {
+  width: 100%;
+}
+.flex-space-around {
+  display: flex;
+  justify-content: space-around;
+}
+.flex-space-between {
+  display: flex;
+  justify-content: space-between;
+}
+.padding-sides {
+  padding: 0 2.5rem;
+}
+a {
   font-size: 2rem;
+  text-decoration: none;
+  margin-bottom: 5px;
+}
+.margin-right {
+  margin-right: 1rem;
 }
 </style>
---
+++ src/components/Login-form.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form action>
+      <inputelement></inputelement>
+    </form>
+  </div>
+</template>
+
+<script>
+import inputelement from "../components/Input-field";
+export default {
+  components: {
+    inputelement: inputelement
+  }
+};
+</script>
+
+<style>
+</style>
