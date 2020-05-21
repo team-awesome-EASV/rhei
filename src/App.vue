cef6fe51cff77ce6e940eeae645f5706bda301bd
darthradek <radoslawhaller@gmail.com>
2020-05-21 13:25:22 +0200

    navigation

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,27 @@
     <div class="main-view">
       <router-view />
     </div>
-    <div id="nav" class="flex-space-around padding-sides">
-      <router-link class="margin-right" to="/moodtracker">Mood</router-link>
-      <router-link class="margin-right" to="/breathing-exercise"
-        >Breath</router-link
-      >
-      <router-link class="margin-right" to="/">Home</router-link>
-      <router-link class="margin-right" to="/chat">Chat</router-link>
-      <router-link class="margin-right" to="/therapy">Therapy</router-link>
+    <div id="nav">
+      <Navigation></Navigation>
+<!--      <router-link class="margin-right" to="/moodtracker">Mood</router-link>-->
+<!--      <router-link class="margin-right" to="/breathing-exercise"-->
+<!--        >Breath</router-link-->
+<!--      >-->
+<!--      <router-link class="margin-right" to="/">Home</router-link>-->
+<!--      <router-link class="margin-right" to="/chat">Chat</router-link>-->
+<!--      <router-link class="margin-right" to="/therapy">Therapy</router-link>-->
     </div>
   </div>
 </template>
+
+<script>
+  import Navigation from "./components/Navigation.vue";
+  export default {
+    components: {
+      Navigation: Navigation
+    }
+  }
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
@@ -37,6 +47,11 @@ html {
   padding: 0;
   border: none;
   outline: none;
+}
+
+body{
+  margin: 0;
+  padding: 0;
 }
 
 #app {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" viewBox="0 0 414 134.157">
+            <defs>
+                <clipPath id="clipped">
+                    <path id="Path" d="M31,0S90.879-2.6,136.863,0s49.084,33.336,74.771,32.689S245.694,1.172,291.146,0,383,0,383,0a31,31,0,0,1,31,31v71a31,31,0,0,1-31,31H31A31,31,0,0,1,0,102V31A31,31,0,0,1,31,0Z" transform="translate(0 1.157)" fill="#eaebf3"/>
+                </clipPath>
+            </defs>
+        </svg>
+
+        <div class="clipped">
+            chuj
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navigation"
+    }
+</script>
+
+<style scoped>
+
+    .clipped {
+        clip-path: url(#clipped);
+        background: green;
+        height:20vh;
+        width:300vh;
+    }
+</style>
