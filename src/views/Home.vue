6891d22c05d0913755e7fc9422047059618d70d7
julia <julia.gru@gmail.com>
2020-05-22 11:20:11 +0200

    illu in progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,18 @@
   <div>
     <h1>this is the Main screen page</h1>
 
-    <p :style="{color : loadUser.hex}">this must have the accent color</p>
+    <p :style="{ color: loadUser.hex }">this must have the accent color</p>
+    <TheCalmDown />
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import TheCalmDown from "../components/illustrations/TheCalmDown";
 export default {
+  components: {
+    TheCalmDown
+  },
   data() {
     return {};
   },
