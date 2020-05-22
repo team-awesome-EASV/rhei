d98a294fb8ba6e90e9e842681db8fb8437a71382
julia <julia.gru@gmail.com>
2020-05-22 15:45:43 +0200

    svg icon wrapper component

--- src/components/AppIcon.vue
+++ src/components/AppIcon.vue
@@ -2,36 +2,37 @@
 
 
 <script>
-    export default {
-        functional: true,
-        props: {
-            icon: {
-                type: String,
-                default: ''
-            },
-        },
-        render(createElement, { props }) {
-            let svgHTML = '';
-            try {
-                svgHTML = require(`!html-loader!@/icons/icon-${props.icon}.svg`);
-            } catch(e) {
-                console.warn(`Unable to load "icon-${props.icon}.svg" icon. Verify it exists in the icons directory.`, e);
-            }
-            return createElement(
-                'span',
-                {
-                    class: `app-icon app-icon-${props.icon}`,
-                    domProps: {
-                        innerHTML: svgHTML
-                    }
-                }
-            );
-        }
-    };
+export default {
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      default: ""
+    }
+  },
+  render(createElement, { props }) {
+    let svgHTML = "";
+    try {
+      svgHTML = require(`!html-loader!@/icons/icon-${props.icon}.svg`);
+    } catch (e) {
+      console.warn(
+        `Unable to load "icon-${props.icon}.svg" icon. Verify it exists in the icons directory.`,
+        e
+      );
+    }
+    return createElement("span", {
+      class: `app-icon app-icon-${props.icon}`,
+      domProps: {
+        innerHTML: svgHTML
+      }
+    });
+  }
+};
 </script>
 
 <style>
-    .app-icon {
-        display: inline-block;
-    }
+.app-icon {
+  display: inline-block;
+  width: 3rem;
+}
 </style>
