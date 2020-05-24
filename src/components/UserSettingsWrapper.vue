8423090448627e1f49de80e4ad79c90fea281fc0
darthradek <radoslawhaller@gmail.com>
2020-05-24 20:52:56 +0200

    user-controls

---
+++ src/components/UserSettingsWrapper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+      <div @click="showControls = true" class="profile-controls-wrapper">
+        <i class="las la-cog picture-cog"></i>
+      </div>
+      <div v-if="showControls" class="profile-controls-popup">
+          <!-- <i class="las la-cog popup-cog"></i>
+          <i class="las la-power-off"></i> -->
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+          showControls: false
+    }
+  }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+.container{
+  position:static;
+}
+ 
+.profile-controls-wrapper{
+  width:10vh;
+  height:10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius:50%;
+  background-color: var(--main-accent-color); 
+  background-image:url("../assets/photos/rick.png");
+  background-size:100% 100%;
+   position:absolute;
+}
+
+.la-cog{
+  color:blue;
+  font-size:40px;
+  margin-top:40px;
+  margin-left:60px;
+}
+
+.profile-controls-popup{
+  height:25vh;
+  width:10vh;
+  border-radius:40px;
+  overflow: hidden;
+  background-color: brown;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="main-padding">
-    <h1>this is the Main screen page</h1>
+    <!-- <h1>this is the Main screen page</h1>
 
     <p :style="{ color: loadUser.hex }">this must have the accent color</p>
     <h2 class="accent-color">
       this must alose be in accents color but with css var
-    </h2>
+    </h2> -->
+  <div class="account-settings-banner">
+     <UserSettingsWrapper />
+  </div>
+  
     <div class="illuWrapper">
       <TheConnectIllu />
     </div>
@@ -15,9 +19,11 @@
 <script>
 import { mapGetters } from "vuex";
 import TheConnectIllu from "../components/illustrations/TheConnectIllu";
+import UserSettingsWrapper from "../components/UserSettingsWrapper"
 export default {
   components: {
-    TheConnectIllu
+    TheConnectIllu,
+    UserSettingsWrapper
   },
   data() {
     return {};
@@ -35,5 +41,15 @@ export default {
 <style>
 .illuWrapper {
   width: 100%;
+}
+
+.account-settings-banner{
+  margin-bottom:70px;
+  width:100%;
+  height:20vh;
+  background: coral ;
+  display: flex;
+  align-items:center;
+  justify-content: flex-end ;
 }
 </style>
