b5337e92c08c6a8160b2e90449eb2f41b1be693f
julia <julia.gru@gmail.com>
2020-05-25 21:48:35 +0200

    calm down programatic nav next and prev

--- src/components/AnimalsFeature.vue
+++ src/components/AnimalsFeature.vue
@@ -1,11 +1,11 @@
 
 <template>
-   <div class="fullscreen main-padding">
-      <div class="arrow-controls">
+  <div class="fullscreen main-padding">
+    <!-- <div class="arrow-controls">
       <router-link to="connect-dots"> 
          <i class="fas fa-arrow-circle-left arrow-size"></i>
       </router-link>
-    </div>
+    </div> -->
     <div class="main-view">
       <h1>Wierd animals feature get sum</h1>
     </div>
--- src/views/CalmDown.vue
+++ src/views/CalmDown.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="calm-down-wrapper">
-    <div class="arrow-container" @click="goToNext()">
+    <div class="arrow-container next" @click="goToNext()">
       <!-- <router-link to="/calm-down/connect-dots">
         
       </router-link> -->
       <i class="fas fa-arrow-circle-right arrow-size"></i>
+    </div>
+    <div class="arrow-container back" @click="goToPrev()">
+      <!-- <router-link to="/calm-down/connect-dots">
+        
+      </router-link> -->
+      <i class="fas fa-arrow-circle-left arrow-size"></i>
     </div>
     <router-view></router-view>
   </div>
@@ -19,6 +25,10 @@ export default {
       else if (this.$route.name === "AnimalsFeature")
         this.$router.push({ name: "Home" });
       else this.$router.push({ name: "connectTheDots" });
+    },
+
+    goToPrev() {
+      this.$router.go(-1);
     }
   }
 };
@@ -28,10 +38,18 @@ export default {
 .calm-down-wrapper {
   position: relative;
 }
-.arrow-container {
+.next {
   position: absolute;
   top: 10;
   right: 0;
+  margin: 1.5rem;
+  z-index: 9;
+}
+
+.back {
+  position: absolute;
+  top: 10;
+  left: 0;
   margin: 1.5rem;
   z-index: 9;
 }
