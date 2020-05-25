09bf4b00927ff864a0b79b451953773bba45fcb9
Johannes <ngiih91@gmail.com>
2020-05-25 15:24:44 +0200

    Rhei video added
    Json file in the assets.

--- src/views/VideoOnboard.vue
+++ src/views/VideoOnboard.vue
@@ -1,12 +1,48 @@
 <template>
   <div>
-    HELLO
+      <div class="item">
+        <lottie
+          :options="defaultOptions"
+          :height="250"
+          :width="250"
+          v-on:animCreated="handleAnimation"
+
+        />
+      </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Lottie from "vue-lottie";
+import * as animationData from "@/assets/video.json";
+
+export default {
+  name: 'juansi',
+    components: {
+    lottie: Lottie
+      },
+
+  data() {
+    return {
+      name: 'juansi',
+      defaultOptions: { animationData: animationData.default },
+      };
+    },
+    methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim;
+    },
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
+#bkgrdVideo {
+  width: 60vw;
+  height: 60vw;
+}
+
+.item {
+  position: absolute,
+}
 </style>
