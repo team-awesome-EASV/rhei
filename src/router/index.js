d9b31a6062437eb26472e059ca6b24c5212176cd
julia <julia.gru@gmail.com>
2020-05-26 21:58:13 +0200

    moodtracker issue

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/MoodTracker.vue'),
+      import(/* webpackChunkName: "about" */ '../views/MoodTrackerOK.vue'),
     children: [
       { path: '/', component: TrackMood, name: 'TrackMood' },
       { path: '/triger-page', component: TrigerPage, name: 'TrigerPage' },
