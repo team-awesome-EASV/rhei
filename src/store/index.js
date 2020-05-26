502acda07b8543aeb3b239e02687037edb4cf738
julia <julia.gru@gmail.com>
2020-05-26 11:07:43 +0200

    store vith vuex presist

--- src/store/index.js
+++ src/store/index.js
@@ -1,123 +1,126 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import VuexPersistence from 'vuex-persist'
 
 
 Vue.use(Vuex)
 
-
-const state = {
-  user: {
-    id: name,
-    name: '',
-    email: '',
-    password: '',
-    access: false,
-    userColor: {
-      hex: '#8A8BFF',
-      hsl: '239'
-    },
-    allMoodPositions: []
-  },
-  accentColor: {
-    hex: '#8A8BFF',
-    hsl: '239'
-  },
-  colors: [
-    {
-      hex: '#FFEA8D',
-      hsl: '48'
-    },
-    {
-      hex: '#FED891',
-      hsl: '39'
-    },
-    {
-      hex: '#FEB18D',
-      hsl: '19'
-    },
-    {
-      hex: '#FC9390',
-      hsl: '1'
-    },
-    {
-      hex: '#FF88F3',
-      hsl: '306'
-    },
-    {
-      hex: '#DE8DFF',
-      hsl: '282'
-    },
-    {
-      hex: '#8A8BFF',
-      hsl: '239'
-    },
-    {
-      hex: '#6CCCFF',
-      hsl: '200'
-    },
-    {
-      hex: '#21F8FD',
-      hsl: '181'
-    },
-    {
-      hex: '#00FFAF',
-      hsl: '161'
-    },
-    {
-      hex: '#00FF65',
-      hsl: '143'
-    },
-    {
-      hex: '#F7C544',
-      hsl: '43'
-    }
-  ]
-}
-
-const getters = {
-  allUser: state => state.users,
-  allColors: state => state.colors,
-  accentColor: state => state.accentColor,
-  moodDotPosition: state => state.user.allMoodPositions
-}
-
-const actions = {
-  setAccentColor({ commit }, color) {
-    const response = color
-    commit('setAccentColor', response)
-  },
-
-  grantAccess({ commit }, value) {
-    const response = value
-    commit('grantAccess', response)
-  },
-  populateMood({ commit }, mood) {
-    const element = mood
-    commit('populateMood', element)
-  },
-  saveUser({ commit }, state) {
-    commit('saveUser', state)
-  }
-
-
-}
-const mutations = {
-  setAccentColor: (state, response) => (state.accentColor = response),
-  setUserColor: (state, response) => (state.user.userColor = response),
-  grantAccess: (state, response) => (state.user.access = response),
-  populateMood(state, element) {
-    state.user.allMoodPositions.push(element);
-  },
-  saveUser: (state) => sessionStorage.setItem(state, JSON.stringify(state)),
-  initaliseStore: (state) => sessionStorage.getItem(state, JSON.parse.state)
-
-}
+// const vuexLocal = new VuexPersistence({
+//   storage: window.localStorage
+// })
 
 export default new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions
+  state: {
+    user: {
+      id: name,
+      name: '',
+      email: '',
+      password: '',
+      access: false,
+      userColor: {
+        hex: '#8A8BFF',
+        hsl: '239'
+      },
+      allMoodPositions: []
+    },
+    accentColor: {
+      hex: '#8A8BFF',
+      hsl: '239'
+    },
+    colors: [
+      {
+        hex: '#FFEA8D',
+        hsl: '48'
+      },
+      {
+        hex: '#FED891',
+        hsl: '39'
+      },
+      {
+        hex: '#FEB18D',
+        hsl: '19'
+      },
+      {
+        hex: '#FC9390',
+        hsl: '1'
+      },
+      {
+        hex: '#FF88F3',
+        hsl: '306'
+      },
+      {
+        hex: '#DE8DFF',
+        hsl: '282'
+      },
+      {
+        hex: '#8A8BFF',
+        hsl: '239'
+      },
+      {
+        hex: '#6CCCFF',
+        hsl: '200'
+      },
+      {
+        hex: '#21F8FD',
+        hsl: '181'
+      },
+      {
+        hex: '#00FFAF',
+        hsl: '161'
+      },
+      {
+        hex: '#00FF65',
+        hsl: '143'
+      },
+      {
+        hex: '#F7C544',
+        hsl: '43'
+      }
+    ]
+  },
+  getters: {
+    allUser: state => state.users,
+    allColors: state => state.colors,
+    accentColor: state => state.accentColor,
+    moodDotPosition: state => state.user.allMoodPositions
+  },
+  mutations: {
+    setAccentColor: (state, response) => (state.accentColor = response),
+    setUserColor: (state, response) => (state.user.userColor = response),
+    grantAccess: (state, response) => (state.user.access = response),
+    populateMood(state, element) {
+      state.user.allMoodPositions.push(element);
+    },
+    saveUser: (state) => sessionStorage.setItem(state, JSON.stringify(state)),
+    initaliseStore: (state) => sessionStorage.getItem(state, JSON.parse.state)
+
+  },
+  actions: {
+    setAccentColor({ commit }, color) {
+      const response = color
+      commit('setAccentColor', response)
+    },
+
+    grantAccess({ commit }, value) {
+      const response = value
+      commit('grantAccess', response)
+    },
+    populateMood({ commit }, mood) {
+      const element = mood
+      commit('populateMood', element)
+    },
+    saveUser({ commit }, state) {
+      commit('saveUser', state)
+    }
+
+
+  },
+  plugins: [new VuexPersistence({ storage: window.sessionStorage, reducer: (state) => ({ user: state.user }) }).plugin]
 })
+
+
+
+
+
+
 
