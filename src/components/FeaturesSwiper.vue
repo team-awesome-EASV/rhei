bf4c6ccaa7166ddecbde58a673b0cc30b16814e5
julia <julia.gru@gmail.com>
2020-05-27 13:46:21 +0200

    swipercss added

--- src/components/FeaturesSwiper.vue
+++ src/components/FeaturesSwiper.vue
@@ -1,78 +1,77 @@
 <template>
-    <!-- Swiper -->
+  <!-- Swiper -->
   <div class="swiper-container">
-      <div class="swiper-wrapper">
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
-          <div class="swiper-slide">Slide 4</div>
-      </div>
-      <!-- Add Pagination -->
-      <div class="swiper-pagination"></div>
-      <!-- Add Arrows -->
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
   </div>
 </template>
 
 <script>
 import Swiper from "swiper";
 
-
 export default {
   name: "timeline",
   data() {
-    return {
-      
-    };
+    return {};
   },
 
   mounted() {
     new Swiper(".swiper-container", {
-    //pagination: '.swiper-pagination',
-    spaceBetween: 30,
-    centeredSlides: true,
-    slidesPerView: 2,
+      //pagination: '.swiper-pagination',
+      spaceBetween: 30,
+      centeredSlides: true,
+      slidesPerView: 2,
 
       autoplay: {
         delay: 3500,
-        disableOnInteraction: true,
+        disableOnInteraction: true
       },
       pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+        el: ".swiper-pagination",
+        clickable: true
       },
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      }
     });
   }
 };
 </script>
 
-<style lang="scss" scoped> 
-     .swiper-container {
-      width: 100%;
-      height: 100%;
-    }
-    .swiper-slide {
-      text-align: center;
-      font-size: 18px;
-      background: #fff;
+<style lang="scss" scoped>
+@import url("../styles/_swiper.scss");
 
-      /* Center slide text vertically */
-      display: -webkit-box;
-      display: -ms-flexbox;
-      display: -webkit-flex;
-      display: flex;
-      -webkit-box-pack: center;
-      -ms-flex-pack: center;
-      -webkit-justify-content: center;
-      justify-content: center;
-      -webkit-box-align: center;
-      -ms-flex-align: center;
-      -webkit-align-items: center;
-      align-items: center;
-    }
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
 </style>
