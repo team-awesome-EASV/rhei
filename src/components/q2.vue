f8f0645e34803743fd01148a2f407ad6b3f8335b
Johannes <ngiih91@gmail.com>
2020-05-28 07:46:14 +0200

    Added quote generator

---
+++ src/components/q2.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+<p>{{ dataQuote2 }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            dataQuote2: {
+                type: String,
+                
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
