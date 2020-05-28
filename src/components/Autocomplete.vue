b25962b2de367bf68d31f48c4ba0e4445d48d6d5
julia <julia.gru@gmail.com>
2020-05-28 13:40:36 +0200

    error fix

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -9,7 +9,9 @@
         @focus="modal = true"
         @input="filterFeelings"
       />
-      <button @click="updateFelings(feeling)" class="rounded-corners-button">Add</button>
+      <button @click="updateFelings(feeling)" class="rounded-corners-button">
+        Add
+      </button>
     </div>
 
     <div v-if="filteredFeelings && modal" class="modal">
@@ -20,7 +22,9 @@
           v-for="(feel, index) in filteredFeelings"
           :key="index"
           @click="setFeeling(feel)"
-        >{{ feel }}</li>
+        >
+          {{ feel }}
+        </li>
       </ul>
     </div>
     <div v-if="allFeelings">
@@ -33,25 +37,7 @@
         >
           {{ feeling }}
           <div v-on:click="deleteElement(index)" class="svg-container">
-<<<<<<< HEAD
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="minus-circle"
-              class="svg-inline--fa fa-minus-circle fa-w-16"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColorgit "
-                d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zM124 296c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h264c6.6 0 12 5.4 12 12v56c0 6.6-5.4 12-12 12H124z"
-              />
-            </svg>
-=======
             <i class="fas fa-backspace" fill="#e43f5a"></i>
->>>>>>> d28e20e074ae8fd05718f036d25d51586a3c3cb9
           </div>
         </li>
       </ul>
@@ -172,7 +158,7 @@ section {
   align-items: center;
 }
 
-.fa-backspace{
-  color:#ffffff;
+.fa-backspace {
+  color: #ffffff;
 }
 </style>
