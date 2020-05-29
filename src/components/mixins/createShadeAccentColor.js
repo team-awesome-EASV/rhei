dcfc1e04b26d2c698c08d8731eaa40bdfb0dbc04
julia <julia.gru@gmail.com>
2020-05-29 15:58:14 +0200

    comments cleanup

--- src/components/mixins/createShadeAccentColor.js
+++ src/components/mixins/createShadeAccentColor.js
@@ -2,19 +2,11 @@ import { mapGetters } from "vuex";
 
 export const createShadeAccentColor = {
 
-
-    data() {
-        return {};
-    },
     computed: {
         ...mapGetters(["userColor"]),
         loadColor() {
             return this.userColor;
         },
-
-        // color1() {
-        //     return `hsl(${this.loadColor.hsl}, 100%, 50%)`;
-        // }
     },
 
     methods: {
